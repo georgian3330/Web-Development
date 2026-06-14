bb8a733cb31d72d1111678f576b05ac82ff7fc16
Sevak Kumar <sevakkumar3330@gmail.com>
2026-06-14 07:05:42 +0530

    Add Mapping Function of ES6

---
+++ 19. ReactJS/11. Mapping Components/src_practice/index.jsx
@@ -0,0 +1,15 @@
+// https://www.emojimeanings.net/list-smileys-people-whatsapp
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/dl
+// https://codesandbox.io/p/sandbox/mapping-components-practice-vqwm2c?file=%2Fsrc%2Findex.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
