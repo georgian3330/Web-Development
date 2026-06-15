3af5e3150549b0fa1b5c9de5d07df463472a0646
Sevak Kumar <sevakkumar3330@gmail.com>
2026-06-15 07:05:18 +0530

    Add Conditional Rendering in React

---
+++ 19. ReactJS/15. Conditional Rendering/src/index.jsx
@@ -0,0 +1,15 @@
+// https://legacy.reactjs.org/docs/conditional-rendering.html#inline-if-with-logical--operator
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator
+// https://en.wikipedia.org/wiki/Single-responsibility_principle
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
