fda5284a016228a47f5b1e4e3431adb8102019c0
Sevak Kumar <sevakkumar3330@gmail.com>
2026-06-16 08:06:12 +0530

    Class Components vs Hooks

---
+++ 19. ReactJS/21. Class Components/src/index.jsx
@@ -0,0 +1,16 @@
+// https://legacy.reactjs.org/docs/hooks-faq.html#should-i-use-hooks-classes-or-a-mix-of-both
+// https://legacy.reactjs.org/docs/hooks-intro.html
+// https://legacy.reactjs.org/docs/state-and-lifecycle.html
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
